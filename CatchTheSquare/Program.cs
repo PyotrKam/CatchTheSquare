979775e6717f60fbad87ae57e0e7ab79eebe64c9
PyotrKam <pyotrk@inbox.lv>
2024-08-19 20:54:23 +0300

    Add Swuare

--- CatchTheSquare/Program.cs
+++ CatchTheSquare/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
 using SFML.Graphics;
 using SFML.Window;
-using SFML.Learning;
+using SFML.System;
+
 
 namespace CatchTheSquare
 {
     class Program 
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            RenderWindow win = new RenderWindow(new VideoMode(800, 600), "Game");
+            win.Closed += Win_closed;
+            win.SetFramerateLimit(60);
 
+            Square square = new Square(new Vector2f(100, 100), 10, new IntRect(0, 0, 800, 600));
+
+            while (win.IsOpen == true)
+            {
+                win.Clear(new Color(230, 230, 230));
+
+                square.Move();
+                square.Draw(win);
+
+                win.DispatchEvents();
+
+                win.Display();
+            }
+
+        }
+
+         static void Win_closed(object sender, EventArgs e)
+        {
+            ((RenderWindow)sender).Close();
         }
     }
 }
--- CatchTheSquare/Square/Square.cs
+++ CatchTheSquare/Square/Square.cs
@@ -1,12 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.System;
 
-namespace CatchTheSquare.Square
+namespace CatchTheSquare
 {
-    class Square
+    public class Square
     {
+        public bool IsActive = true;
+
+        private RectangleShape shape;
+        private float movementSpeed;
+        private Vector2f movementTarget;
+        private IntRect movementBounds;
+
+        public Square(Vector2f position, float movementSpeed, IntRect movemetBounds) 
+        {
+            shape = new RectangleShape(new Vector2f(100, 100));
+            shape.Position = position;
+
+            this.movementSpeed = movementSpeed;
+            this.movementBounds = movemetBounds;
+
+            UpdateMovementTarget();        
+        
+        }
+
+        public void Move()
+        {
+            Vector2f dir = movementTarget - shape.Position;
+            float magnitude = (float) Math.Sqrt(dir.X * dir.X + dir.Y * dir.Y);
+
+            if (magnitude <= movementSpeed)
+            {
+                shape.Position = movementTarget;
+            }
+            else
+            {
+                shape.Position += dir / magnitude * movementSpeed;
+            }
+
+            
+
+            if (shape.Position == movementTarget)
+            {
+                UpdateMovementTarget();
+            }
+
+        }
+
+        public void Draw(RenderWindow win) 
+        {
+            if (IsActive == false) return;
+
+            win.Draw(shape);
+        
+        }
+
+        private void UpdateMovementTarget() 
+        {
+            Random rnd = new Random();
+
+            movementTarget.X = rnd.Next(movementBounds.Left, movementBounds.Left + movementBounds.Width);
+            movementTarget.Y = rnd.Next(movementBounds.Top, movementBounds.Top + movementBounds.Height);
+        
+        }
+
+        
     }
 }
