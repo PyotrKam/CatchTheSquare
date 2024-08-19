ecd2419e0908bdd56d2d46ddb6fad27bf7ea0be1
PyotrKam <pyotrk@inbox.lv>
2024-08-19 20:03:15 +0300

    Добавьте файлы проекта.

---
+++ CatchTheSquare/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.Window;
+using SFML.Learning;
+
+namespace CatchTheSquare
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
