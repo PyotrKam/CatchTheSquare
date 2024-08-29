fdb30d6f7caad4b81529659cb872970928a1746c
PyotrKam <pyotrk@inbox.lv>
2024-08-29 22:14:35 +0300

    Add_Sprites

---
+++ CatchTheSquare/Sprite/EnemySprite.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.System;
+
+namespace CatchTheSquare
+{
+    class EnemySprite : Square
+    {
+        static Texture EnemyTexture;
+                
+        private static float MaxMovementSpeed = 3;
+        private static float MovementStep = 0.05f;
+        private static float maxSpriteSize = 150;
+        private static float spriteSizeStep = 1f;
+
+        public EnemySprite(Vector2f position, float movementSpeed, IntRect movemetBounds) : base(position, movementSpeed, movemetBounds)
+        {
+            EnemyTexture = new Texture("pirate.png");
+            shapeSprite = new Sprite(EnemyTexture);
+            shapeSprite.Position = position;
+        }
+
+        public override void Move()
+        {
+            shapeSprite.Position = Mathf.MoveTowards(shapeSprite.Position, movementTarget, movementSpeed);
+
+            if (shapeSprite.Position == movementTarget)
+            {
+                OnReachedTarget();
+                UpdateMovementTarget();
+            }
+        }
+        public override void Draw(RenderWindow win)
+        {
+            base.Draw(win);
+
+            win.Draw(shapeSprite);
+        }
+        public override void CheckMousePosition(Vector2i mousePos)
+        {
+            base.CheckMousePosition(mousePos);
+
+            if (mousePos.X > shape.Position.X && mousePos.X < shape.Position.X + shape.Size.X
+                && mousePos.Y > shape.Position.Y && mousePos.Y < shape.Position.Y + shape.Size.Y) OnClick();
+        }
+
+        protected override void OnClick()
+        {
+            Game.IsLost = true;
+        }
+
+        protected override void OnReachedTarget()
+        {
+            if (movementSpeed < MaxMovementSpeed) movementSpeed += MovementStep;
+
+            if (shapeSprite.Scale.X < maxSpriteSize) shapeSprite.Scale += new Vector2f(spriteSizeStep, spriteSizeStep);
+        }
+
+    }
+}
---
+++ CatchTheSquare/Sprite/PlayerSprite.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatchTheSquare
+{
+    public class PlayerSprite : Square
+    {
+        static Texture PlayerTexture;
+        private static float SizeStep = 10f;
+        private static float MinSize = 30f;
+
+        public PlayerSprite(Vector2f position, float movementSpeed, IntRect movemetBounds) : base (position, movementSpeed, movemetBounds)
+        {
+            PlayerTexture = new Texture("smile.png");
+            shapeSprite = new Sprite(PlayerTexture);
+            shapeSprite.Position = position;            
+        }
+        public override void Move()
+        {
+            shapeSprite.Position = Mathf.MoveTowards(shapeCircle.Position, movementTarget, movementSpeed);
+
+            if (shapeSprite.Position == movementTarget)
+            {    
+                UpdateMovementTarget();
+            }
+        }
+
+        public override void Draw(RenderWindow win)
+        {
+            base.Draw(win);
+
+            win.Draw(shapeSprite);
+        }
+
+        public override void CheckMousePosition(Vector2i mousePos)
+        {
+            base.CheckMousePosition(mousePos);
+
+            if (mousePos.X > shapeSprite.Position.X && mousePos.X < shapeSprite.Position.X + shapeSprite.Texture.Size.X &&
+                mousePos.Y > shapeSprite.Position.Y && mousePos.Y < shapeSprite.Position.Y + shapeSprite.Texture.Size.Y)
+                OnClick();
+
+        }
+
+        protected override void OnClick()
+        {
+            Game.Scores++;
+
+            shapeSprite.Scale -= new Vector2f(SizeStep, SizeStep); ;
+
+            if (shapeSprite.Scale.X < MinSize)
+            {
+                IsActive = false;
+                return;
+            }
+
+            UpdateMovementTarget();
+            shapeSprite.Position = movementTarget;
+        }
+
+    }
+}
